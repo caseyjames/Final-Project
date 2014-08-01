02a8e76aa913547df7c771601d464df10d1ceab3
captcontrol <captcontrol35@gmail.com>
2014-07-31 19:22:17 -0600

    Commented everything and fixed spellcheckFile skipping first word.

--- Assignments/src/FinalProject/TextProcessor.java
+++ Assignments/src/FinalProject/TextProcessor.java
@@ -5,82 +5,93 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
-/**
- *
- *
- *
- *
- */
-@SuppressWarnings("UnusedDeclaration")
-public class TextProcessor {
 
+/**
+ * @author Casey Nordgran
+ * @author Tom Pridham
+ *         <p/>
+ *         <p/>
+ *         Text Processor class that has a spellchecker that can check individual words or entire files.
+ *         Returning a file with all the incorrectly spelled words replaced with their correct spellings.
+ *         It also has a file compressor and decompressor that will compress or decompress a passed file
+ *         and write it to the specified output file.
+ *         It can also simulate trasnmitting a file over a network.
+ *         All interaction is done via commandline.
+ */
+public class TextProcessor {
     private static Dictionary dictionary;
     private static Compressor compressor;
     private static Decompressor decompressor;
+    private static DeviceManager dev;
+    private static String newLine = System.lineSeparator();
 
+    /**
+     * Main method of Text Processor.  Initializes all the components necessary
+     * for the methods to function and then asks the user for input.
+     *
+     * @param args0 - dictionary file with word frequencies
+     */
     public static void main(String[] args0) {
-//        initializeComponents(args0[0]);
-        initializeComponents("wordstats1.txt");
+
+        initializeComponents(args0[0]);
         if (dictionary == null)
             return;
 
         Scanner scanner = new Scanner(new InputStreamReader(System.in));
-        Scanner lineParse;
-        String input, input1, input2;
-        String[] ins;
+        String input, input2;
 
+        //main input loop
         while (true) {
             System.out.println("Please choose from the following options:");
             System.out.println("1) Word spell check");
             System.out.println("2) File spell check");
             System.out.println("3) File compression");
             System.out.println("4) File decompression");
-            System.out.println("5) File remote transfer\n");
+            System.out.println("5) File remote transfer" + newLine);
             input = scanner.next();
 
+            //spellcheckWord
             if (input.equals("1")) {
                 System.out.println("Please enter a text word: ");
                 input = scanner.next();
                 input2 = scanner.nextLine();
+                //checks whether verbose or not
                 if (input2.equals(" -f"))
                     spellcheckWord(input, true);
                 else
                     spellcheckWord(input, false);
+
+                //spellcheckFile
             } else if (input.equals("2")) {
                 System.out.println("Please enter the source file path: ");
                 input = scanner.next();
                 System.out.println("Please enter the destination file path: ");
                 input2 = scanner.next();
                 spellcheckFile(input, input2);
+
+                //compressFile
             } else if (input.equals("3")) {
                 System.out.println("Please enter the source file path: ");
                 input = scanner.next();
-                File inputFile = new File(input);
-                if (!inputFile.isFile()) {
-                    System.out.println(input + "is invalid for compression!\n");
-                    continue;
-                }
                 System.out.println("Please enter the destination file path: ");
                 input2 = scanner.next();
                 compressFile(input, input2);
 
+                //decompressFile
             } else if (input.equals("4")) {
                 System.out.println("Please enter the source file path: ");
                 input = scanner.next();
-                File inputFile = new File(input);
-                if (!inputFile.isFile()) {
-                    System.out.println(input + "is invalid for decompression!\n");
-                    continue;
-                }
                 System.out.println("Please enter the destination file path: ");
                 input2 = scanner.next();
                 decompressFile(input, input2);
 
+                //transmitFile
             } else if (input.equals("5")) {
                 System.out.println("Please enter the source file path: ");
                 input = scanner.next();
-                transmitFile(input, args0[0]);
+                transmitFile(input, "");
 
+                //check exit or validity of entry
             } else {
                 if (input.equals("exit")) {
                     System.out.println("Thanks for using the text processor...");
@@ -90,25 +101,27 @@ public class TextProcessor {
                 }
             }
         }
-
     }
 
+    /**
+     * Sets up the necessary components for the rest of the program to run
+     *
+     * @param statsFile - dictionary file with word frequencies
+     */
     public static void initializeComponents(String statsFile) {
+        //check validity of file
         File inputFile = new File(statsFile);
         if (!inputFile.isFile()) {
-            System.out.println("Invalid word stats file argument!\n");
+            System.out.println("Invalid word stats file argument!" + newLine);
             return;
         }
 
+        //create dictionary and compressors
         dictionary = new Dictionary(inputFile, 2000);
         compressor = new Compressor();
         decompressor = new Decompressor();
-// This driver method should first create a file from statsFile and check it for validity, then it should instantiates
-// your spell checker component using the file, your compression component, and the device manager that is newly given below.
-// Since your program is interactive, it is best to initialize all three in this method once for the remainder of the life of the program.
-// If the statsFile is invalid then it should print the following message and return: Invalid word stats file argument!
-// Please note that the above method should only be called once for the remainder of the life of the program. The rest of
-// these following methods below will be called as many times as the user asks for with their respective option.
+        dev = new DeviceManager();
+
     }
 
     public static void spellcheckWord(String word, boolean fileWrite) {
@@ -123,65 +136,78 @@ public class TextProcessor {
         }
     }
 
+    /**
+     * Checks a passed file for misspelled words and returns a new file with all the
+     * misspelled words replaced with their correct spelling.
+     * @param srcFile - file to check, <infile>.ext
+     * @param dstFile - file to write, <outfile>.ext
+     */
     public static void spellcheckFile(String srcFile, String dstFile) {
-        int message = 0;
+
+        //check file for validity
         File inputFile = new File(srcFile);
         if (!inputFile.isFile()) {
-            System.out.println(srcFile + "is invalid for spell correction!\n");
+            System.out.println(srcFile + "is invalid for spell correction!" + newLine);
             return;
         }
+
         Scanner inputFileLine;
-        try {
-            inputFileLine = new Scanner(inputFile);
-        } catch (Exception e) {
-            System.err.println(e.getMessage());
-            return;
-        }
-//        File inputF = new File(dstFile);
         PrintWriter outputFile;
         try {
+            inputFileLine = new Scanner(inputFile);
             outputFile = new PrintWriter(dstFile);
         } catch (Exception e) {
             System.err.println(e.getMessage());
             return;
         }
 
+        int message = 0;
+
+        //while there are still lines in the file to be read
         while (inputFileLine.hasNextLine()) {
-            String currentLine = inputFileLine.nextLine();
-            String[] tokens = currentLine.split("\\b");
+            //get next line and split into tokens
+            String[] tokens = inputFileLine.nextLine().split("\\b");
             String alternateWord;
-            for (int i = 1; i < tokens.length; i++) {
-                if (Character.isAlphabetic((tokens[i].codePointAt(0))) || Character.isDigit(tokens[i].charAt(0))) {
-                    alternateWord = dictionary.spellCheck(tokens[i], false);
-                    if (alternateWord != null) {
-                        if (alternateWord.equals(tokens[i]))
-                            outputFile.print(alternateWord);
-                        else if (alternateWord.equals("")) {
-                            message = 2;
-                            outputFile.print(tokens[i]);
-                        } else {
-                            if (message != 2)
-                                message = 1;
-                            outputFile.print(alternateWord);
-                        }
+            //for every token in the current line
+            for (String token : tokens) {
+                if (token.length() == 0)
+                    continue;
+                //if the token is a word
+                if (Character.isAlphabetic((token.codePointAt(0))) || Character.isDigit(token.charAt(0))) {
+                    //check spelling
+                    alternateWord = dictionary.spellCheck(token, false);
+                    //if no alternates, print original token
+                    if (alternateWord.equals("")) {
+                        message = 2;
+                        outputFile.print(token);
+                        //else print corrected word
+                    } else {
+                        if (message != 2)
+                            message = 1;
+                        outputFile.print(alternateWord);
                     }
                 } else {
-                    outputFile.print(tokens[i]);
+                    outputFile.print(token);
                 }
             }
-            outputFile.print("\n");
+            outputFile.println();
         }
         outputFile.close();
 
+        //print message declaring success
         if (message == 0) {
-            System.out.println(srcFile + " contains words with correct spelling!\n\n");
-//            inputF.delete();
+            System.out.println(srcFile + " contains words with correct spelling!" + newLine);
         } else if (message == 1)
-            System.out.println(srcFile + " was corrected successfully!\n\n");
+            System.out.println(srcFile + " was corrected successfully!" + newLine);
         else
-            System.out.println(srcFile + " was corrected, but it contains unknown words!\n\n");
+            System.out.println(srcFile + " was corrected, but it contains unknown words!" + newLine);
     }
 
+    /**
+     * Compresses a text file using the Huffman algorithm.
+     * @param srcFile - file to check, <infile>.ext
+     * @param dstFile - file to write, <outfile>.ext
+     */
     public static void compressFile(String srcFile, String dstFile) {
         File inFile = new File(srcFile);
         File outFile = new File(dstFile);
@@ -198,6 +224,11 @@ public class TextProcessor {
         }
     }
 
+    /**
+     * Decompresses a text file using the Huffman algorithm.
+     * @param srcFile - file to check, <infile>.ext
+     * @param dstFile - file to write, <outfile>.ext
+     */
     public static void decompressFile(String srcFile, String dstFile) {
         File inFile = new File(srcFile);
         File outFile = new File(dstFile);
@@ -214,9 +245,12 @@ public class TextProcessor {
         }
     }
 
+    /**
+     *
+     * @param srcFile - file to transfer, <infile>.ext
+     * @param statsFile - unused
+     */
     public static void transmitFile(String srcFile, String statsFile) {
-
-        DeviceManager dev = new DeviceManager();
 
         dev.initializeNetwork();
         dev.transmitFile(srcFile);
