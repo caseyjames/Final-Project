ef30c6a8628c05251009b636295dcefe7fd09b07
captcontrol <captcontrol35@gmail.com>
2014-08-01 19:33:38 -0600

    Added tests for compress and decompress

--- Assignments/src/FinalProject/TextProcessorTester.java
+++ Assignments/src/FinalProject/TextProcessorTester.java
@@ -1,8 +1,54 @@
 package FinalProject;
 
+import java.io.File;
+import java.io.IOException;
+
 /**
  * @author Casey Nordgran
  * @author Tom Pridham
  */
 public class TextProcessorTester {
+    public void testCompressor() {
+        Compressor comp = new Compressor();
+        File test = new File("wordstats1.txt");
+        File out = new File("out.txt");
+        try {
+            comp.compress(test, out);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        assert (out.exists());
+        assert (test.length() > out.length());
+
+        File junk = null;
+        File outJunk = null;
+        try {
+            comp.compress(junk, outJunk);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void testDecompressor() {
+        Decompressor comp = new Decompressor();
+        File test = new File("out.txt");
+        File out = new File("check.txt");
+        try {
+            comp.decompress(test, out);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        assert (out.exists());
+        assert (test.length() < out.length());
+
+        File junk = null;
+        File outJunk = null;
+        try {
+            comp.decompress(junk, outJunk);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
